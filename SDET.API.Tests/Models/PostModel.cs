1afdacc86db1d58c73a29a5df44fb1f3ff5f5469
circletakesthesquare <13kochd@gmail.com>
2025-12-05 14:03:49 -0500

    Add nullable for string type

--- SDET.API.Tests/Models/PostModel.cs
+++ SDET.API.Tests/Models/PostModel.cs
@@ -5,7 +5,7 @@ namespace SDET.API.Tests.Models
     {
         public int UserId { get; set; }
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Body { get; set; }
+        public string? Title { get; set; }
+        public string? Body { get; set; }
     }
 }
