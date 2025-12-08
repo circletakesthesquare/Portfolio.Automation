94eff4be32ce978f51d02b91ae8e97feebbbbe99
circletakesthesquare <13kochd@gmail.com>
2025-12-08 13:18:20 -0500

    Add static endpoints class, update Postsclient to reference these endpoints instead of hardcoding

---
+++ SDET.API.Tests/Utilities/Endpoints.cs
@@ -0,0 +1,11 @@
+namespace SDET.API.Tests.Utilities
+{
+    public static class Endpoints
+    {
+        // Posts endpoints
+        public static string GetPost(int id) => $"/posts/{id}";
+        public static string CreatePost => "/posts";
+        public static string UpdatePost(int id) => $"/posts/{id}";
+        public static string DeletePost(int id) => $"/posts/{id}";
+    }
+}
