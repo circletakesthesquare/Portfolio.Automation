5c89f2e2b187708af81a56a9d463259614c98cd0
circletakesthesquare <13kochd@gmail.com>
2025-12-11 17:56:30 -0500

    Add comment endpoints

--- API.Tests/Utilities/Endpoints.cs
+++ API.Tests/Utilities/Endpoints.cs
@@ -7,5 +7,16 @@ namespace API.Tests.Utilities
         public static string CreatePost => "/posts";
         public static string UpdatePost(int id) => $"/posts/{id}";
         public static string DeletePost(int id) => $"/posts/{id}";
+
+
+        // Comments endpoints
+        public static string GetComment(int id) => $"/comments/{id}";
+        public static string GetAllComments => "/comments";
+        public static string GetCommentsForPost(int postId) => $"/posts/{postId}/comments";
+        public static string GetCommentsByPostId(int postId) => $"/comments?postId={postId}";
+        public static string GetCommentsByEmail(string email) => $"/comments?email={email}";
+        public static string CreateComment => "/comments";
+        public static string UpdateComment(int id) => $"/comments/{id}";
+        public static string DeleteComment(int id) => $"/comments/{id}";
     }
 }
