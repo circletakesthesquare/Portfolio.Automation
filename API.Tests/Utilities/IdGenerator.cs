e3ed19fa820016a3336750ea9e8444f70bdfd1c1
circletakesthesquare <13kochd@gmail.com>
2025-12-11 17:56:05 -0500

    Update IdGenerator and Ranges to support comment Id ranges

--- API.Tests/Utilities/IdGenerator.cs
+++ API.Tests/Utilities/IdGenerator.cs
@@ -16,4 +16,16 @@ public static class IdGenerator
     /// </summary>
     /// <returns>A random invalid Post ID.</returns>
     public static int RandomInvalidPostId() => _random.Next(IdRanges.MinInvalidPostId, IdRanges.MaxInvalidPostId + 1);
+
+    /// <summary>
+    /// Generates a random valid Comment ID within the defined range.
+    /// </summary>
+    /// <returns>A random valid Comment ID.</returns>
+    public static int RandomValidCommentId() => _random.Next(IdRanges.MinValidCommentId, IdRanges.MaxValidCommentId + 1);
+
+    /// <summary>
+    /// Generates a random invalid Comment ID within the defined range.
+    /// </summary>
+    /// <returns>A random invalid Comment ID.</returns>
+    public static int RandomInvalidCommentId() => _random.Next(IdRanges.MinInvalidCommentId, IdRanges.MaxInvalidCommentId + 1);
 }
--- API.Tests/Utilities/IdRanges.cs
+++ API.Tests/Utilities/IdRanges.cs
@@ -3,8 +3,15 @@
 /// </summary>
 public static class IdRanges
 {
+    // Post ID Ranges
     public const int MinValidPostId = 1;
     public const int MaxValidPostId = 100;
     public const int MinInvalidPostId = -100;
     public const int MaxInvalidPostId = 0;
+
+    // Comment ID Ranges
+    public const int MinValidCommentId = 1;
+    public const int MaxValidCommentId = 500;
+    public const int MinInvalidCommentId = -500;
+    public const int MaxInvalidCommentId = 0;
 }
