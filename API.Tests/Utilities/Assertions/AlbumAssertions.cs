a045327c75ade28a835cb4411edc1f2161206d8e
circletakesthesquare <13kochd@gmail.com>
2026-01-06 17:21:34 -0500

    Create custom assertions for albums

---
+++ API.Tests/Utilities/Assertions/AlbumAssertions.cs
@@ -0,0 +1,42 @@
+
+
+using System.Reflection.Emit;
+using Api.Tests.Models;
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace API.Tests.Utilities
+{
+    public static class AlbumAssertions
+    {
+        /// <summary>
+        /// Asserts that the actual Album matches the expected Album.
+        /// </summary>
+        /// <param name="actual">The actual Album object created from the response.</param>
+        /// <param name="expected">The expected Album object created from the request.</param>
+        public static void ShouldMatch(this Album? actual, Album expected)
+        {
+            using (new AssertionScope())
+            {
+                // handle case where actual is null
+                ArgumentNullException.ThrowIfNull(actual);
+
+                actual.Id.Should().BePositive("Expected album ID to be positive.");
+                actual.UserId.Should().BePositive("Expected album user ID to be positive.");
+                actual.Title.Should().Be(expected.Title, "Expected album Title to match.");
+                
+            }
+        }
+        
+        public static void ShouldExist(this Album? album, int id)
+        {
+            album.Should().NotBeNull("Expected album to exist, but it was null.");
+            album!.Id.Should().Be(id, "Expected album ID to match the provided ID.");
+        }
+
+        public static void ShouldBeDeleted(this Album? album)
+        {
+            album.Should().BeNull("Expected album to not exist, but it was found.");
+        }
+    }
+}
